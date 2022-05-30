0dfdcdd63bc8873f5f8d8a678177a44054eed122
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-05-30 15:01:41 +0200

    working on multiple paragraphs

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-let test = ["T", "e", "s", "t"]
+let test = ['T', 'e', 's', 't']
 
-function easyRead (){
-    let p = document.querySelectorAll("p").innerHTML
-    console.log(p)
-    let words = p.split(" ")
-    let paragraph = document.createElement("p")
-    for (let word of words){
-        console.log(word)
-        let letters = word.split("")
-        let first = firstHalf(letters)
-        let second = secondHalf(letters)
-        paragraph.innerHTML += (first)
-        paragraph.append(second)
-        paragraph.innerHTML += " "
-    }
-
-    return paragraph
+function easyRead (p) {
+  para = p.innerHTML
+  let words = para.split(' ')
+  let paragraph = document.createElement('p')
+  for (let word of words) {
+    console.log(word)
+    let letters = word.split('')
+    let first = firstHalf(letters)
+    let second = secondHalf(letters)
+    paragraph.innerHTML += first
+    paragraph.append(second)
+    paragraph.innerHTML += ' '
+  }
+  return paragraph
 }
 
-function firstHalf(array){
-    let bold = []
-    bold.push("<b>")
-    for (let i = 0; i < (array.length / 2); i++){
-        bold.push(array[i])
-    }
-    bold.push("</b>")
-    return bold.join("")
+function firstHalf (array) {
+  let bold = []
+  bold.push('<b>')
+  for (let i = 0; i < array.length / 2; i++) {
+    bold.push(array[i])
+  }
+  bold.push('</b>')
+  return bold.join('')
 }
 
-function secondHalf(array){
-    let slim = []
-    for (let i = array.length; i >= (array.length / 2); i--){
-        slim.push(array[i])
-    }
-    return slim.reverse().join("")
+function secondHalf (array) {
+  let slim = []
+  for (let i = array.length; i >= array.length / 2; i--) {
+    slim.push(array[i])
+  }
+  return slim.reverse().join('')
 }
 
-function renderEasyRead (){
-    let body = document.querySelector("body")
-    let paragraph = easyRead()
-    body.innerHTML = ""
-    body.append(paragraph)
+function renderEasyRead () {
+  let body = document.querySelector('body')
+  let paragraphs = document.querySelectorAll('p')
+  body.innerHTML = ''
+  for (let p of paragraphs){
+  let paragraph = easyRead(p)
+  body.append(paragraph)
+  }
 }
